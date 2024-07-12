7bf7b743de2613bf57ff2698b3ef3eaca127dadc
Luke Sexton <luke.sexton@krakenflex.com>
2024-07-12 11:31:43 +1000

    feat: add mp report structure and request args for report list

--- soap/src/lib/requests/index.mjs
+++ soap/src/lib/requests/index.mjs
@@ -1,9 +1,11 @@
+import { MPReport } from "./mp-report/index.mjs";
 import { MPMarket } from "./mp-market/index.mjs";
 
 export class CombinedRequests {
   constructor() {
     this.mpMarket = new MPMarket();
-    this.requests = [...this.mpMarket.requests];
+    this.mpReport = new MPReport();
+    this.requests = [...this.mpMarket.requests, ...this.mpReport.requests];
   }
 
   getArgs = (lib, fn) => this[lib][fn]();
---
+++ soap/src/lib/requests/mp-report/index.mjs
@@ -0,0 +1,22 @@
+import { baseArgs } from "../../../constants/index.mjs";
+
+export class MPReport {
+  constructor() {
+    this.name = "mpReport";
+    this.requestType = "mp.report";
+    this.partialArgs = { requestType: this.requestType, ...baseArgs }; // Order of request args matters
+    this.requests = [
+      {
+        fn: "getReportListArgs",
+        lib: this.name,
+        name: "mp-report-report-list",
+      },
+    ];
+  }
+
+  getReportListArgs = () => ({
+    ...this.partialArgs,
+    requestData:
+      "PE1hcmtldFJlcG9ydCBQYXJ0aWNpcGFudE5hbWU9IkYxMDAiIEFwcGxpY2F0aW9uVHlwZT0iTUFSS0VUX1JFUE9SVCI+Cgk8UmVwb3J0TGlzdFJlcXVlc3QgUmVwb3J0VHlwZT0iUkVHSVNUUkFUSU9OIiBSZXBvcnRTdWJUeXBlPSJSRVNPVVJDRSIgUGVyaW9kaWNpdHk9Ik9OX0RFTUFORCIgRGF0ZT0iMjAyNC0wNy0xMSI+Cgk8L1JlcG9ydExpc3RSZXF1ZXN0Pgo8L01hcmtldFJlcG9ydD4=",
+  });
+}
